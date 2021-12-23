0dad8c119cee402f0e38193d9b41c123c02e30a4
AttitudefuckerXD <95916276+AttitudefuckerXD@users.noreply.github.com>
2021-12-23 18:49:58 +0530

    Update forcesubs.py

--- EmikoRobot/modules/forcesubs.py
+++ EmikoRobot/modules/forcesubs.py
@@ -205,7 +205,7 @@ But then you have many groups, and you don't want this spammer to be in one of y
 *No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
 You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
 *Commands:*\n
-Feds are now divided into 3 sections for your ease.\n 
+Feds are now divided into 3 sections for your ease.\n • `/newfed <fed_name>`*:* Creates a Federation, One allowed per user
 • `/fedownerhelp`*:* Provides help for fed creation and owner only commands
 • `/fedadminhelp`*:* Provides help for fed administration commands
 • `/feduserhelp`*:* Provides help for commands anyone can use
