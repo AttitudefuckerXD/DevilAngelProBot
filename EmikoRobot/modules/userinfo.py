61cb55c69f7777ee901bcccf69237e9d80fcae18
AttitudefuckerXD <95916276+AttitudefuckerXD@users.noreply.github.com>
2021-12-24 15:15:04 +0530

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -445,9 +445,9 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>╔═━「 Current Devil Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "<b>╔═━「 Current 𝙳ᴇᴠɪʟ✗𝙰ɳɠɛƖ Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>╘═━「 Powered By kennedy-ex 」</b>"
+    result += "\n<b>╘═━「 Powered By 𝐏𝐲𝐓𝐠𝐌𝐮𝐬𝐢𝐜⚡ 」</b>"
     update.effective_message.reply_text(
         result,
         parse_mode=ParseMode.HTML, 
