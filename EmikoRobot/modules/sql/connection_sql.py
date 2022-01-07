2e78833e4410027126f1c9b50ab3004eb296eca3
AttitudefuckerXD <95916276+AttitudefuckerXD@users.noreply.github.com>
2022-01-07 19:18:13 +0530

    Update connection_sql.py

--- EmikoRobot/modules/sql/connection_sql.py
+++ EmikoRobot/modules/sql/connection_sql.py
@@ -1,209 +1,245 @@
 import threading
-import time
-from typing import Union
 
-from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
-from sqlalchemy.sql.sqltypes import BigInteger
+from sqlalchemy import Boolean, Column, UnicodeText
 
-from EmikoRobot.modules.sql import SESSION, BASE
+from EmikoRobot.modules.sql import BASE, SESSION
 
 
-class ChatAccessConnectionSettings(BASE):
-    __tablename__ = "access_connection"
-    chat_id = Column(String(14), primary_key=True)
-    allow_connect_to_chat = Column(Boolean, default=True)
+class CleanerBlueTextChatSettings(BASE):
+    __tablename__ = "cleaner_bluetext_chat_setting"
+    chat_id = Column(UnicodeText, primary_key=True)
+    is_enable = Column(Boolean, default=False)
 
-    def __init__(self, chat_id, allow_connect_to_chat):
-        self.chat_id = str(chat_id)
-        self.allow_connect_to_chat = str(allow_connect_to_chat)
+    def __init__(self, chat_id, is_enable):
+        self.chat_id = chat_id
+        self.is_enable = is_enable
 
     def __repr__(self):
-        return "<Chat access settings ({}) is {}>".format(
-            self.chat_id,
-            self.allow_connect_to_chat,
-        )
+        return "clean blue text for {}".format(self.chat_id)
 
 
-class Connection(BASE):
-    __tablename__ = "connection"
-    user_id = Column(BigInteger, primary_key=True)
-    chat_id = Column(String(14))
+class CleanerBlueTextChat(BASE):
+    __tablename__ = "cleaner_bluetext_chat_ignore_commands"
+    chat_id = Column(UnicodeText, primary_key=True)
+    command = Column(UnicodeText, primary_key=True)
 
-    def __init__(self, user_id, chat_id):
-        self.user_id = user_id
-        self.chat_id = str(chat_id)  # Ensure String
-
-
-class ConnectionHistory(BASE):
-    __tablename__ = "connection_history"
-    user_id = Column(BigInteger, primary_key=True)
-    chat_id = Column(String(14), primary_key=True)
-    chat_name = Column(UnicodeText)
-    conn_time = Column(Integer)
-
-    def __init__(self, user_id, chat_id, chat_name, conn_time):
-        self.user_id = user_id
-        self.chat_id = str(chat_id)
-        self.chat_name = str(chat_name)
-        self.conn_time = int(conn_time)
-
-    def __repr__(self):
-        return "<connection user {} history {}>".format(self.user_id, self.chat_id)
+    def __init__(self, chat_id, command):
+        self.chat_id = chat_id
+        self.command = command
 
 
-ChatAccessConnectionSettings.__table__.create(checkfirst=True)
-Connection.__table__.create(checkfirst=True)
-ConnectionHistory.__table__.create(checkfirst=True)
+class CleanerBlueTextGlobal(BASE):
+    __tablename__ = "cleaner_bluetext_global_ignore_commands"
+    command = Column(UnicodeText, primary_key=True)
 
-CHAT_ACCESS_LOCK = threading.RLock()
-CONNECTION_INSERTION_LOCK = threading.RLock()
-CONNECTION_HISTORY_LOCK = threading.RLock()
-
-HISTORY_CONNECT = {}
+    def __init__(self, command):
+        self.command = command
 
 
-def allow_connect_to_chat(chat_id: Union[str, int]) -> bool:
-    try:
-        chat_setting = SESSION.query(ChatAccessConnectionSettings).get(str(chat_id))
-        if chat_setting:
-            return chat_setting.allow_connect_to_chat
-        return False
-    finally:
-        SESSION.close()
+CleanerBlueTextChatSettings.__table__.create(checkfirst=True)
+CleanerBlueTextChat.__table__.create(checkfirst=True)
+CleanerBlueTextGlobal.__table__.create(checkfirst=True)
+
+CLEANER_CHAT_SETTINGS = threading.RLock()
+CLEANER_CHAT_LOCK = threading.RLock()
+CLEANER_GLOBAL_LOCK = threading.RLock()
+
+CLEANER_CHATS = {}
+GLOBAL_IGNORE_COMMANDS = set()
 
 
-def set_allow_connect_to_chat(chat_id: Union[int, str], setting: bool):
-    with CHAT_ACCESS_LOCK:
-        chat_setting = SESSION.query(ChatAccessConnectionSettings).get(str(chat_id))
-        if not chat_setting:
-            chat_setting = ChatAccessConnectionSettings(chat_id, setting)
+def set_cleanbt(chat_id, is_enable):
+    with CLEANER_CHAT_SETTINGS:
+        curr = SESSION.query(CleanerBlueTextChatSettings).get(str(chat_id))
+        if curr:
+            SESSION.delete(curr)
 
-        chat_setting.allow_connect_to_chat = setting
-        SESSION.add(chat_setting)
+        newcurr = CleanerBlueTextChatSettings(str(chat_id), is_enable)
+
+        SESSION.add(newcurr)
         SESSION.commit()
 
 
-def connect(user_id, chat_id):
-    with CONNECTION_INSERTION_LOCK:
-        prev = SESSION.query(Connection).get((int(user_id)))
-        if prev:
-            SESSION.delete(prev)
-        connect_to_chat = Connection(int(user_id), chat_id)
-        SESSION.add(connect_to_chat)
-        SESSION.commit()
-        return True
+def chat_ignore_command(chat_id, ignore):
+    ignore = ignore.lower()
+    with CLEANER_CHAT_LOCK:
+        ignored = SESSION.query(CleanerBlueTextChat).get((str(chat_id), ignore))
 
+        if not ignored:
 
-def get_connected_chat(user_id):
-    try:
-        return SESSION.query(Connection).get((int(user_id)))
-    finally:
-        SESSION.close()
+            if str(chat_id) not in CLEANER_CHATS:
+                CLEANER_CHATS.setdefault(
+                    str(chat_id),
+                    {"setting": False, "commands": set()},
+                )
 
+            CLEANER_CHATS[str(chat_id)]["commands"].add(ignore)
 
-def curr_connection(chat_id):
-    try:
-        return SESSION.query(Connection).get((str(chat_id)))
-    finally:
-        SESSION.close()
-
-
-def disconnect(user_id):
-    with CONNECTION_INSERTION_LOCK:
-        disconnect = SESSION.query(Connection).get((int(user_id)))
-        if disconnect:
-            SESSION.delete(disconnect)
+            ignored = CleanerBlueTextChat(str(chat_id), ignore)
+            SESSION.add(ignored)
             SESSION.commit()
             return True
         SESSION.close()
         return False
 
 
-def add_history_conn(user_id, chat_id, chat_name):
-    global HISTORY_CONNECT
-    with CONNECTION_HISTORY_LOCK:
-        conn_time = int(time.time())
-        if HISTORY_CONNECT.get(int(user_id)):
-            counting = (
-                SESSION.query(ConnectionHistory.user_id)
-                    .filter(ConnectionHistory.user_id == str(user_id))
-                    .count()
-            )
-            getchat_id = {}
-            for x in HISTORY_CONNECT[int(user_id)]:
-                getchat_id[HISTORY_CONNECT[int(user_id)][x]["chat_id"]] = x
-            if chat_id in getchat_id:
-                todeltime = getchat_id[str(chat_id)]
-                delold = SESSION.query(ConnectionHistory).get(
-                    (int(user_id), str(chat_id)),
+def chat_unignore_command(chat_id, unignore):
+    unignore = unignore.lower()
+    with CLEANER_CHAT_LOCK:
+        unignored = SESSION.query(CleanerBlueTextChat).get((str(chat_id), unignore))
+
+        if unignored:
+
+            if str(chat_id) not in CLEANER_CHATS:
+                CLEANER_CHATS.setdefault(
+                    str(chat_id),
+                    {"setting": False, "commands": set()},
                 )
-                if delold:
-                    SESSION.delete(delold)
-                    HISTORY_CONNECT[int(user_id)].pop(todeltime)
-            elif counting >= 5:
-                todel = list(HISTORY_CONNECT[int(user_id)])
-                todel.reverse()
-                todel = todel[4:]
-                for x in todel:
-                    chat_old = HISTORY_CONNECT[int(user_id)][x]["chat_id"]
-                    delold = SESSION.query(ConnectionHistory).get(
-                        (int(user_id), str(chat_old)),
-                    )
-                    if delold:
-                        SESSION.delete(delold)
-                        HISTORY_CONNECT[int(user_id)].pop(x)
-        else:
-            HISTORY_CONNECT[int(user_id)] = {}
-        delold = SESSION.query(ConnectionHistory).get((int(user_id), str(chat_id)))
-        if delold:
-            SESSION.delete(delold)
-        history = ConnectionHistory(int(user_id), str(chat_id), chat_name, conn_time)
-        SESSION.add(history)
-        SESSION.commit()
-        HISTORY_CONNECT[int(user_id)][conn_time] = {
-            "chat_name": chat_name,
-            "chat_id": str(chat_id),
-        }
+            if unignore in CLEANER_CHATS.get(str(chat_id)).get("commands"):
+                CLEANER_CHATS[str(chat_id)]["commands"].remove(unignore)
+
+            SESSION.delete(unignored)
+            SESSION.commit()
+            return True
+
+        SESSION.close()
+        return False
 
 
-def get_history_conn(user_id):
-    if not HISTORY_CONNECT.get(int(user_id)):
-        HISTORY_CONNECT[int(user_id)] = {}
-    return HISTORY_CONNECT[int(user_id)]
+def global_ignore_command(command):
+    command = command.lower()
+    with CLEANER_GLOBAL_LOCK:
+        ignored = SESSION.query(CleanerBlueTextGlobal).get(str(command))
+
+        if not ignored:
+            GLOBAL_IGNORE_COMMANDS.add(command)
+
+            ignored = CleanerBlueTextGlobal(str(command))
+            SESSION.add(ignored)
+            SESSION.commit()
+            return True
+
+        SESSION.close()
+        return False
 
 
-def clear_history_conn(user_id):
-    global HISTORY_CONNECT
-    todel = list(HISTORY_CONNECT[int(user_id)])
-    for x in todel:
-        chat_old = HISTORY_CONNECT[int(user_id)][x]["chat_id"]
-        delold = SESSION.query(ConnectionHistory).get((int(user_id), str(chat_old)))
-        if delold:
-            SESSION.delete(delold)
-            HISTORY_CONNECT[int(user_id)].pop(x)
-    SESSION.commit()
-    return True
+def global_unignore_command(command):
+    command = command.lower()
+    with CLEANER_GLOBAL_LOCK:
+        unignored = SESSION.query(CleanerBlueTextGlobal).get(str(command))
+
+        if unignored:
+            if command in GLOBAL_IGNORE_COMMANDS:
+                GLOBAL_IGNORE_COMMANDS.remove(command)
+
+            SESSION.delete(command)
+            SESSION.commit()
+            return True
+
+        SESSION.close()
+        return False
 
 
-def __load_user_history():
-    global HISTORY_CONNECT
+def is_command_ignored(chat_id, command):
+    if command.lower() in GLOBAL_IGNORE_COMMANDS:
+        return True
+
+    if str(chat_id) in CLEANER_CHATS:
+        if command.lower() in CLEANER_CHATS.get(str(chat_id)).get("commands"):
+            return True
+
+    return False
+
+
+def is_enabled(chat_id):
     try:
-        qall = SESSION.query(ConnectionHistory).all()
-        HISTORY_CONNECT = {}
-        for x in qall:
-            check = HISTORY_CONNECT.get(x.user_id)
-            if check is None:
-                HISTORY_CONNECT[x.user_id] = {}
-            HISTORY_CONNECT[x.user_id][x.conn_time] = {
-                "chat_name": x.chat_name,
-                "chat_id": x.chat_id,
-            }
+        resultcurr = SESSION.query(CleanerBlueTextChatSettings).get(str(chat_id))
+        if resultcurr:
+            return resultcurr.is_enable
+        return False  # default
     finally:
         SESSION.close()
 
 
-__load_user_history()
+def get_all_ignored(chat_id):
+    if str(chat_id) in CLEANER_CHATS:
+        LOCAL_IGNORE_COMMANDS = CLEANER_CHATS.get(str(chat_id)).get("commands")
+    else:
+        LOCAL_IGNORE_COMMANDS = set()
 
+    return GLOBAL_IGNORE_COMMANDS, LOCAL_IGNORE_COMMANDS
+
+
+def __load_cleaner_list():
+    global GLOBAL_IGNORE_COMMANDS
+    global CLEANER_CHATS
+
+    try:
+        GLOBAL_IGNORE_COMMANDS = {
+            (x.command) for x in SESSION.query(CleanerBlueTextGlobal).all()
+        }
+    finally:
+        SESSION.close()
+
+    try:
+        for x in SESSION.query(CleanerBlueTextChatSettings).all():
+            CLEANER_CHATS.setdefault(x.chat_id, {"setting": False, "commands": set()})
+            CLEANER_CHATS[x.chat_id]["setting"] = x.is_enable
+    finally:
+        SESSION.close()
+
+    try:
+        for x in SESSION.query(CleanerBlueTextChat).all():
+            CLEANER_CHATS.setdefault(x.chat_id, {"setting": False, "commands": set()})
+            CLEANER_CHATS[x.chat_id]["commands"].add(x.command)
+    finally:
+        SESSION.close()
+
+
+__load_cleaner_list()
+
+
+
+
+
+
+
+    
+   
+
+
+
+    
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+                    
 
     
     
